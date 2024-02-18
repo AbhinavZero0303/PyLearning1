9376dabd95221e877a2d748d5eec89c50f5f9970
AbhinavZero0303 <mishraabhinav0398@gmail.com>
2024-02-18 17:22:38 +0530

    Second commit

---
+++ Task 1st week/Task1.py
@@ -0,0 +1,10 @@
+# Take input for user details
+
+name = input("Enter your name")
+age = input("Enter your age")
+roll = input("Enter your roll_number")
+ph = input("Enter your phone_number")
+print("Your name is", name)
+print("Your age is", age)
+print("Your roll number is", roll)
+print("Your phone number is", ph)
