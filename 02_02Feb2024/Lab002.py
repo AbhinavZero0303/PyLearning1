3165ffe5c3497bdc4988d68edf01568292485d55
AbhinavZero0303 <mishraabhinav0398@gmail.com>
2024-02-18 14:28:34 +0530

    18 Feb 2024

---
+++ 02_02Feb2024/Lab002.py
@@ -0,0 +1,21 @@
+age = 45
+print(age)
+age = "Abhinav"
+print(age)
+age = 'C'
+print(age)
+_123 = 789
+print(_123)
+_ = "Sachin"
+print(_)
+
+print("Pramod")
+print("Pramod", 25, 'C')
+print("Pramod", 25, 'C', sep="_")
+print("Pramod", 25, 'C', sep="_", end="$")
+print("Pramod", 25, 'C')
+
+result = max(6, 9)
+print(result)
+
+
