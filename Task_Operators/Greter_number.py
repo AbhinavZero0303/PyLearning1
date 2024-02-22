3c566a6105becd96efe5c69e9147f27603ee6fb4
AbhinavZero0303 <mishraabhinav0398@gmail.com>
2024-02-22 15:48:21 +0530

    task 2nd

---
+++ Task_Operators/Greter_number.py
@@ -0,0 +1,11 @@
+num_1 = int(input("Enter a number:"))
+num_2 = int(input("Enter a number"))
+if num_1 > num_2:
+    print("num_1 is greater")
+if num_1 < num_2:
+    print("num_1 is smaller")
+if num_1 == num_2:
+    print("num_1 is equal")
+
+
+
---
+++ Task_Operators/Square_Cube.py
@@ -0,0 +1,5 @@
+num =float(input("Enter the number:"))
+square = num * num
+cube = num * num * num
+print("Square is: ", square)
+print("Cube root is: ", cube)
